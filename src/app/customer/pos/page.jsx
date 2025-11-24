c7aae55520e98bdfb2597b1d26a63a4837cd271b
Fayeq <faqib7488@gmail.com>
2025-11-23 19:22:10 -0800

    final-2

--- src/app/customer/pos/page.jsx
+++ src/app/customer/pos/page.jsx
@@ -196,239 +196,238 @@ const Page = () => {
   }
 
   return (
-    <div>f</div>
-    // <div className="flex gap-3 h-[88vh] ">
-    //   <Card className="w-[120mm] rounded-2xl h-full  hidden p-3  lg:flex lg:flex-col lg:justify-between ">
-    //     <Table className={" text-xs"}>
-    //       {cart.length === 0 && (
-    //         <TableCaption className="w-full text-center p-2">
-    //           با افزودن یک محصول آغاز کنید.{" "}
-    //         </TableCaption>
-    //       )}
+    <div className="flex gap-3 h-[88vh] ">
+      <Card className="w-[120mm] rounded-2xl h-full  hidden p-3  lg:flex lg:flex-col lg:justify-between ">
+        <Table className={" text-xs"}>
+          {cart.length === 0 && (
+            <TableCaption className="w-full text-center p-2">
+              با افزودن یک محصول آغاز کنید.{" "}
+            </TableCaption>
+          )}
 
-    //       <TableBody>
-    //         {cart.map((item, i) => (
-    //           <TableRow key={i} className={"text-center"}>
-    //             <TableCell className={""}>
-    //               <div className="flex flex-col gap-1 w-40 overflow-hidden  p-1 rounded-md ">
-    //                 <span className="leading-none w-40 text-wrap">
-    //                   {" "}
-    //                   {item.product?.name || item.product}
-    //                 </span>
-    //                 <Separator className={"bg-chart-1"} />
-    //                 <span>{item?.badgeNumber}</span>
-    //               </div>
-    //             </TableCell>
-    //             <TableCell>
-    //               <div className="flex flex-col gap-1">
-    //                 <span className="bg-sidebar-accent p-1 rounded-xl">
-    //                   {item.unit?.name || item.unit}
-    //                 </span>
-    //                 <span> {item?.count}</span>
-    //               </div>
-    //             </TableCell>
-    //             <TableCell>
-    //               <div className="flex flex-col gap-1">
-    //                 <span className="p-1 rounded-xl bg-chart-6">
-    //                   {formatCurrency(item.saleAmount)}
-    //                 </span>
-    //                 <span className="p-1 rounded-xl bg-chart-1">
-    //                   {formatCurrency(item.saleAmount * item.count)}
-    //                 </span>
-    //               </div>
-    //             </TableCell>
+          <TableBody>
+            {cart.map((item, i) => (
+              <TableRow key={i} className={"text-center"}>
+                <TableCell className={""}>
+                  <div className="flex flex-col gap-1 w-40 overflow-hidden  p-1 rounded-md ">
+                    <span className="leading-none w-40 text-wrap">
+                      {" "}
+                      {item.product?.name || item.product}
+                    </span>
+                    <Separator className={"bg-chart-1"} />
+                    <span>{item?.badgeNumber}</span>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <div className="flex flex-col gap-1">
+                    <span className="bg-sidebar-accent p-1 rounded-xl">
+                      {item.unit?.name || item.unit}
+                    </span>
+                    <span> {item?.count}</span>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <div className="flex flex-col gap-1">
+                    <span className="p-1 rounded-xl bg-chart-6">
+                      {formatCurrency(item.saleAmount)}
+                    </span>
+                    <span className="p-1 rounded-xl bg-chart-1">
+                      {formatCurrency(item.saleAmount * item.count)}
+                    </span>
+                  </div>
+                </TableCell>
 
-    //             <TableCell>
-    //               <Button
-    //                 onClick={() => handleRemoveitemFromCart(item._id)}
-    //                 variant="ghost"
-    //               >
-    //                 <Trash className="text-red-400" />
-    //               </Button>
-    //             </TableCell>
-    //           </TableRow>
-    //         ))}
-    //       </TableBody>
-    //     </Table>
-    //     <div className="space-y-2">
-    //       <div className="flex gap-2 text-sm">
-    //         <Label className={"w-1/3 font-extrabold"}> فیصدی تخفیف : </Label>
-    //         <Input
-    //           className={"h-8"}
-    //           placeholder="فیصدی تخفیف"
-    //           type={"number"}
-    //           value={cent}
-    //           onChange={(e) =>
-    //             setCent(Math.min(Math.max(Number(e.target.value), 0), 100))
-    //           }
-    //         />
-    //       </div>
-    //       <Card>
-    //         <CardContent className={"space-y-2 text-sm"}>
-    //           <CardTitle className={"flex justify-between"}>
-    //             <span>تعداد :</span> <span>{formatNumber(totalCount)}</span>
-    //           </CardTitle>
-    //           <CardTitle className={"flex justify-between"}>
-    //             <span>مجموع :</span> <span>{formatCurrency(totalAmount)}</span>
-    //           </CardTitle>
-    //           <CardTitle className={"flex justify-between"}>
-    //             <span>تخفیف :</span> <span>{formatCurrency(discount)}</span>
-    //           </CardTitle>
-    //           <CardTitle className={"flex justify-between"}>
-    //             <span>قابل پرداخت :</span>{" "}
-    //             <span>{formatCurrency(totalAmount - discount)}</span>
-    //           </CardTitle>
-    //           <div className="flex gap-2 items-center justify-between">
-    //             <AutoCompleteV2
-    //               className={"w-27 h-8"}
-    //               label="خریدار"
-    //               value={buyer}
-    //               onChange={setBuyer}
-    //               type={"buyer"}
-    //             />
-    //             <Input
-    //               className={"w-27 h-8"}
-    //               placeholder=" نقد"
-    //               type={"number"}
-    //               value={cashAmount}
-    //               onChange={(e) =>
-    //                 setCashAmount(Math.max(Number(e.target.value), 0))
-    //               }
-    //             />
-    //             <CardTitle className={""}>
-    //               <span>باقی :</span>{" "}
-    //               <span>
-    //                 {formatCurrency(totalAmount - discount - cashAmount || 0)}
-    //               </span>
-    //             </CardTitle>
-    //           </div>
-    //           <div className="flex gap-2">
-    //             <Button
-    //               className={"flex-1"}
-    //               disabled={!cart.length}
-    //               onClick={() => handlePay(true)}
-    //             >
-    //               {isPending2 ? (
-    //                 <Loader2Icon className="animate-spin" />
-    //               ) : (
-    //                 "   پرداخت و چاپ بل "
-    //               )}
-    //             </Button>
-    //             <Button
-    //               variant={"outline"}
-    //               className={"shadow-lg shadow-black/80 hover:shadow-md"}
-    //               onClick={() => handlePay(false)}
-    //               disabled={!cart.length}
-    //             >
-    //               {isPending2 ? (
-    //                 <Loader2Icon className="animate-spin" />
-    //               ) : (
-    //                 "پرداخت"
-    //               )}
-    //             </Button>
-    //           </div>
-    //         </CardContent>
-    //       </Card>
-    //     </div>
-    //   </Card>
-    //   <div className="w-full lg:w-[calc(100%-120mm)] space-y-2">
-    //     <div className="flex gap-2 w-full">
-    //       <Input
-    //         value={search}
-    //         onChange={(e) => setSearch(e.target.value)}
-    //         className={"rounded-2xl"}
-    //         placeholder={"جستجو ......."}
-    //       />
-    //       <form onSubmit={handleSubmitBarCode}>
-    //         <Input
-    //           value={barCode}
-    //           onChange={(e) => setBarCode(e.target.value)}
-    //           className={"rounded-2xl"}
-    //           placeholder={"بارکود"}
-    //           autoFocus
-    //         />
-    //       </form>
-    //     </div>
-    //     {isPending ? (
-    //       <div className="size-full h-[calc(100%-40px)] flex justify-center items-center">
-    //         <Loader2Icon className="animate-spin" />
-    //       </div>
-    //     ) : (
-    //       <Card
-    //         className={
-    //           " bg-my-card w-full shadow-2xl   rounded-2xl h-[calc(100%-40px)] grid 2xl:grid-cols-4 xl:grid-cols-3   lg:grid-cols-1 md:grid-cols-2 grid-cols-1 p-3 overflow-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] z"
-    //         }
-    //       >
-    //         {results.map((item, i) => {
-    //           const exiset = getQunatity(item._id);
+                <TableCell>
+                  <Button
+                    onClick={() => handleRemoveitemFromCart(item._id)}
+                    variant="ghost"
+                  >
+                    <Trash className="text-red-400" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <div className="space-y-2">
+          <div className="flex gap-2 text-sm">
+            <Label className={"w-1/3 font-extrabold"}> فیصدی تخفیف : </Label>
+            <Input
+              className={"h-8"}
+              placeholder="فیصدی تخفیف"
+              type={"number"}
+              value={cent}
+              onChange={(e) =>
+                setCent(Math.min(Math.max(Number(e.target.value), 0), 100))
+              }
+            />
+          </div>
+          <Card>
+            <CardContent className={"space-y-2 text-sm"}>
+              <CardTitle className={"flex justify-between"}>
+                <span>تعداد :</span> <span>{formatNumber(totalCount)}</span>
+              </CardTitle>
+              <CardTitle className={"flex justify-between"}>
+                <span>مجموع :</span> <span>{formatCurrency(totalAmount)}</span>
+              </CardTitle>
+              <CardTitle className={"flex justify-between"}>
+                <span>تخفیف :</span> <span>{formatCurrency(discount)}</span>
+              </CardTitle>
+              <CardTitle className={"flex justify-between"}>
+                <span>قابل پرداخت :</span>{" "}
+                <span>{formatCurrency(totalAmount - discount)}</span>
+              </CardTitle>
+              <div className="flex gap-2 items-center justify-between">
+                <AutoCompleteV2
+                  className={"w-27 h-8"}
+                  label="خریدار"
+                  value={buyer}
+                  onChange={setBuyer}
+                  type={"buyer"}
+                />
+                <Input
+                  className={"w-27 h-8"}
+                  placeholder=" نقد"
+                  type={"number"}
+                  value={cashAmount}
+                  onChange={(e) =>
+                    setCashAmount(Math.max(Number(e.target.value), 0))
+                  }
+                />
+                <CardTitle className={""}>
+                  <span>باقی :</span>{" "}
+                  <span>
+                    {formatCurrency(totalAmount - discount - cashAmount || 0)}
+                  </span>
+                </CardTitle>
+              </div>
+              <div className="flex gap-2">
+                <Button
+                  className={"flex-1"}
+                  disabled={!cart.length}
+                  onClick={() => handlePay(true)}
+                >
+                  {isPending2 ? (
+                    <Loader2Icon className="animate-spin" />
+                  ) : (
+                    "   پرداخت و چاپ بل "
+                  )}
+                </Button>
+                <Button
+                  variant={"outline"}
+                  className={"shadow-lg shadow-black/80 hover:shadow-md"}
+                  onClick={() => handlePay(false)}
+                  disabled={!cart.length}
+                >
+                  {isPending2 ? (
+                    <Loader2Icon className="animate-spin" />
+                  ) : (
+                    "پرداخت"
+                  )}
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </Card>
+      <div className="w-full lg:w-[calc(100%-120mm)] space-y-2">
+        <div className="flex gap-2 w-full">
+          <Input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className={"rounded-2xl"}
+            placeholder={"جستجو ......."}
+          />
+          <form onSubmit={handleSubmitBarCode}>
+            <Input
+              value={barCode}
+              onChange={(e) => setBarCode(e.target.value)}
+              className={"rounded-2xl"}
+              placeholder={"بارکود"}
+              autoFocus
+            />
+          </form>
+        </div>
+        {isPending ? (
+          <div className="size-full h-[calc(100%-40px)] flex justify-center items-center">
+            <Loader2Icon className="animate-spin" />
+          </div>
+        ) : (
+          <Card
+            className={
+              " bg-my-card w-full shadow-2xl   rounded-2xl h-[calc(100%-40px)] grid 2xl:grid-cols-4 xl:grid-cols-3   lg:grid-cols-1 md:grid-cols-2 grid-cols-1 p-3 overflow-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] z"
+            }
+          >
+            {results.map((item, i) => {
+              const exiset = getQunatity(item._id);
 
-    //           return (
-    //             <Card
-    //               key={i}
-    //               className={
-    //                 "p-2   shadow-xl border-[1px] border-chart-1 border-dashed  hover:shadow-2xl hover:bg-sidebar-border transition-all duration-300 h-min"
-    //               }
-    //             >
-    //               <div className="relative w-full h-[140px]  shadow-[2px_5px_20px_-1px_rgba(0,0,0,0.3)] rounded-md border-0 overflow-hidden  ">
-    //                 <Image src={item?.product?.image} fill objectFit="cover" />
-    //               </div>
-    //               <CardContent className={"space-y-2 p-0"}>
-    //                 <CardTitle className={"text-md "}>
-    //                   {item?.product?.name}
-    //                 </CardTitle>
-    //                 <div className="flex justify-between">
-    //                   <CardTitle className={"text-sm"}>
-    //                     {formatCurrency(item.saleAmount)}{" "}
-    //                   </CardTitle>
-    //                   <CardTitle className={"text-sm"}>
-    //                     {formatNumber(item.count)}{" "}
-    //                     <span className="italic text-xs text-green-700 ms-1">
-    //                       {item?.unit?.name}
-    //                     </span>{" "}
-    //                   </CardTitle>
-    //                 </div>
-    //                 <div className="flex justify-between items-center">
-    //                   {!exiset ? (
-    //                     <Button
-    //                       className={"px-10"}
-    //                       size={"sm"}
-    //                       onClick={() => handleAddItemToCart(item)}
-    //                     >
-    //                       افزودن
-    //                     </Button>
-    //                   ) : (
-    //                     <div className="flex gap-1">
-    //                       <Button
-    //                         size={"sm"}
-    //                         onClick={() => handleAddQuantity(item._id)}
-    //                       >
-    //                         <Plus />
-    //                       </Button>
-    //                       <Input
-    //                         className={"size-[33px] px-1 "}
-    //                         value={exiset}
-    //                         onChange={(e) =>
-    //                           handleChangeInput(e.target.value, item._id)
-    //                         }
-    //                       />
-    //                       <Button
-    //                         size={"sm"}
-    //                         onClick={() => handleMinusQuantity(item._id)}
-    //                       >
-    //                         <Minus />
-    //                       </Button>
-    //                     </div>
-    //                   )}
-    //                   <CardDescription>{item.badgeNumber}</CardDescription>
-    //                 </div>
-    //               </CardContent>
-    //             </Card>
-    //           );
-    //         })}
-    //       </Card>
-    //     )}
-    //   </div>
-    // </div>
+              return (
+                <Card
+                  key={i}
+                  className={
+                    "p-2   shadow-xl border-[1px] border-chart-1 border-dashed  hover:shadow-2xl hover:bg-sidebar-border transition-all duration-300 h-min"
+                  }
+                >
+                  <div className="relative w-full h-[140px]  shadow-[2px_5px_20px_-1px_rgba(0,0,0,0.3)] rounded-md border-0 overflow-hidden  ">
+                    <Image src={item?.product?.image} fill objectFit="cover" />
+                  </div>
+                  <CardContent className={"space-y-2 p-0"}>
+                    <CardTitle className={"text-md "}>
+                      {item?.product?.name}
+                    </CardTitle>
+                    <div className="flex justify-between">
+                      <CardTitle className={"text-sm"}>
+                        {formatCurrency(item.saleAmount)}{" "}
+                      </CardTitle>
+                      <CardTitle className={"text-sm"}>
+                        {formatNumber(item.count)}{" "}
+                        <span className="italic text-xs text-green-700 ms-1">
+                          {item?.unit?.name}
+                        </span>{" "}
+                      </CardTitle>
+                    </div>
+                    <div className="flex justify-between items-center">
+                      {!exiset ? (
+                        <Button
+                          className={"px-10"}
+                          size={"sm"}
+                          onClick={() => handleAddItemToCart(item)}
+                        >
+                          افزودن
+                        </Button>
+                      ) : (
+                        <div className="flex gap-1">
+                          <Button
+                            size={"sm"}
+                            onClick={() => handleAddQuantity(item._id)}
+                          >
+                            <Plus />
+                          </Button>
+                          <Input
+                            className={"size-[33px] px-1 "}
+                            value={exiset}
+                            onChange={(e) =>
+                              handleChangeInput(e.target.value, item._id)
+                            }
+                          />
+                          <Button
+                            size={"sm"}
+                            onClick={() => handleMinusQuantity(item._id)}
+                          >
+                            <Minus />
+                          </Button>
+                        </div>
+                      )}
+                      <CardDescription>{item.badgeNumber}</CardDescription>
+                    </div>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </Card>
+        )}
+      </div>
+    </div>
   );
 };
 
